2c2f95b80d7be8877754af4fe32c24b4245296e7
D <nilkantha.dipesh@gmail.com>
2018-02-06 17:34:38 +0545

    Fixed stage not found error

--- src/stages/mainStage.js
+++ src/stages/mainStage.js
@@ -8,12 +8,12 @@ class MainStage{
 	constructor(){
 		this.stage = new PIXI.Container;
 		this.stage.interactive=true;
-		this.bat1 = new Bat(0, 0x16a085, true,stage);
-		this.bat2 = new Bat(CONFIG.width-10, 0x16a085, true,stage);
+		this.bat1 = new Bat(0, 0x16a085, true, this.stage);
+		this.bat2 = new Bat(CONFIG.width-10, 0x16a085, true, this.stage);
 		this.bat1.attach_ball();
 		this.ticker = new PIXI.ticker.Ticker();
 		this.ticker.add(this.game_loop);
-		this.stage.addChild( bat1.get_bat(), bat2.get_bat());
+		this.stage.addChild( this.bat1.get_bat(), this.bat2.get_bat());
 	}
 
 	// get the stage
@@ -28,5 +28,6 @@ class MainStage{
 
 
 let mainStage = new MainStage();
+let stage = mainStage.get_stage();
 
-export default mainStage.get_stage;
+export default stage;
